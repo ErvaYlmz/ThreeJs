df233f7146422940cf2de21864d6c5433182d798
Nurerva Yılmaz <130571315+ErvaYlmz@users.noreply.github.com>
2024-01-20 15:19:19 +0300

    Update Assignment5.js

--- Assignment5.js
+++ Assignment5.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
 import { OBJLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/OBJLoader.js";
 
 const w = window.innerWidth;
@@ -7,17 +7,28 @@ const h = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
+camera.position.y = 1;
+camera.position.x = -3;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-renderer.setClearColor(new THREE.Color(0x87CEEB));
+renderer.setClearColor(new THREE.Color(0x87ceeb));
+
+const groundTexture = new THREE.TextureLoader().load(
+  "./texture-grass-field_1232-251.jpg"
+);
+scene.background = groundTexture;
+
+const dogTexture = new THREE.TextureLoader().load("./dog-120.png");
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
-function init (geometry) { 
-  const material = new THREE.MeshMatcapMaterial();
+function init(geometry) {
+  const material = new THREE.MeshMatcapMaterial({
+    //map: dogTexture,
+  });
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
 
@@ -30,19 +41,45 @@ function init (geometry) {
   filllight.position.y = -2;
   scene.add(filllight);
 
-  function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-  }
   animate();
 }
 
-const loader = new OBJLoader();
-loader.load("./spider.obj", (obj) => init(obj.children[0].geometry) );
+function initMovingObject() {
+  const radius = 0.35;
+  const sphereGeometry = new THREE.SphereGeometry(radius, 32, 32);
+  const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  scene.add(sphere);
 
-function handleWindowResize () {
+  sphere.position.set(15, 1, 0);
+  const speed = 0.15;
+
+  function animateSphere() {
+    sphere.position.x -= speed;
+
+    // If the sphere is out of the view, remove it
+    if (sphere.position.x < -10) {
+      scene.remove(sphere);
+    }
+    requestAnimationFrame(animateSphere);
+  }
+  animateSphere();
+}
+
+const loader = new OBJLoader();
+loader.load("./dog.obj", (obj) => init(obj.children[0].geometry));
+
+initMovingObject();
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+function handleWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
-window.addEventListener('resize', handleWindowResize, false);
+window.addEventListener("resize", handleWindowResize, false);
+animate();
