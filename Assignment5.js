1ea866cdb336dc72732be923e7087bf91a8c53a3
Nurerva Yılmaz <130571315+ErvaYlmz@users.noreply.github.com>
2024-01-21 18:53:43 +0300

    Update Assignment5.js

--- Assignment5.js
+++ Assignment5.js
@@ -1,17 +1,4 @@
-// yapmamız gerekenler:
-// 1. kameranın sabit kalmasını sağlamak (yapıldı)
-// 2. köpeği sadece yukarı-aşağı hareket ettirmek (yapıldı ????)
-// 3. köpek ve topun üst üste gelince topun yok olmasını sağlamak (initMovingObject fonksiyonunda if condition ekledim ama nedense olmuyo)
-// 4. yakalanan top sayına göre puan eklemek (optional)
-// 5. toplara png eklemek (optional) ve daha iyi bi arka plan fotoğrafı bulunabilir
-
-
-// bu arada js in işleyişine pek hakim değilsen şundan haberdar edeyim:
-// c deki printf("%d",dogPositionX); yerine console.log(dogPositionX); kullanabilirsin
-// sonra web sayfasında f12 yapıp console a girip print edip etmediğine bakabilirsin
-
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
 import { OBJLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/OBJLoader.js";
 
 const w = window.innerWidth-5;
@@ -37,18 +24,10 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-//ground Texture : arka plan fotoğrafı
-
 const groundTexture = new THREE.TextureLoader().load(
   "./marble.jpg"
 );
 scene.background = groundTexture;
-
-//dog texture : köpeğin arka planı
-const dogTexture = new THREE.TextureLoader().load("./dog-120.png");
-
-// const controls = new OrbitControls(dog, renderer.domElement); // camera idi
-// controls.update();
 
 let dogPositionY=0;
 let dogPositionX=-5;
@@ -56,7 +35,7 @@ let dogPositionZ = 0;
 
 function init(geometry) {
   const material = new THREE.MeshMatcapMaterial({
-    //map: dogTexture,
+    // color: some color in hex
   });
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
@@ -111,7 +90,7 @@ function initMovingObject() {
     // toplam puan 150'yi geçince top üretimi durdur ve ekrana "Good Job!" yaz
     if(point > 150){
         clearInterval(intervalId);
-        document.getElementById('container').innerHTML="Good Job!";
+        document.getElementById('game-over').style.zIndex=5;
     }
 
     requestAnimationFrame(animateSphere);
@@ -121,12 +100,6 @@ function initMovingObject() {
 
 // Her 3 saniyede bir top üret
 intervalId = setInterval(initMovingObject, 3000);
-
-// Top üretmeyi durdurmak için bu satırı kullanabilirsiniz:
-// clearInterval(intervalId);
-
-
-
 
 const loader = new OBJLoader();
 loader.load("./dog.obj", (obj) => init(obj.children[0].geometry));
